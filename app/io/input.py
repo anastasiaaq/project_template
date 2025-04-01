d99aaeba25bd01748c403b7b4d90db028c27cd48
anastasiaaq <ndvojlenko@gmail.com>
2025-04-01 16:41:27 +0300

    Created empty function for file input and output

---
+++ app/io/input.py
@@ -0,0 +1,17 @@
+def get_text_from_console():
+    """
+    Функція для введення тексту з консолі.
+    """
+    pass
+
+def read_text_from_file_builtin(filepath):
+    """
+    Функція для зчитування тексту з файлу за допомогою вбудованих можливостей Python.
+    """
+    pass
+
+def read_text_from_file_pandas(filepath):
+    """
+    Функція для зчитування тексту з файлу за допомогою бібліотеки pandas.
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,11 @@
+def print_text_to_console(text):
+    """
+    Функція для виведення тексту в консоль.
+    """
+    pass
+
+def write_text_to_file_builtin(filepath, text):
+    """
+    Функція для запису тексту до файлу за допомогою вбудованих можливостей Python.
+    """
+    pass
